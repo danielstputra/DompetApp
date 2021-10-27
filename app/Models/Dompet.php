64e2b2974c9eac1659198df6b9df78efeb84d4b5
danielstputra <danielstputra@gmail.com>
2021-10-27 20:00:37 +0700

    Fix model dompet

--- app/Models/Dompet.php
+++ app/Models/Dompet.php
@@ -11,7 +11,7 @@ class Dompet extends Model
 
     public $table = "dompet";
 
-    protected $primaryKey = 'id';
+    protected $primaryKey = 'dompet_id';
     
     public $timestamps = true;
     
@@ -21,9 +21,9 @@ class Dompet extends Model
      * @var array
      */
     protected $fillable = [
-        'name',
-        'reference',
-        'description',
+        'dompet_name',
+        'dompet_reference',
+        'dompet_description',
         'dompet_status_id'
     ];
 }
