acf665c2a97b1bc515e65e35713a3fe3af77aeb4
danielstputra <danielstputra@gmail.com>
2021-10-27 18:07:19 +0700

    Tambah attribute pada model Dompet

--- app/Models/Dompet.php
+++ app/Models/Dompet.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Dompet extends Model
 {
     use HasFactory;
+
+    public $table = "dompet";
+
+    protected $primaryKey = 'id';
+    
+    public $timestamps = true;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'reference',
+        'description',
+        'dompet_status_id'
+    ];
 }
