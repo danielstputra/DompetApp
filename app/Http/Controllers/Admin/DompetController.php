ec838c8ef83f7d9cf89286a7913f73b07d23dbaa
danielstputra <danielstputra@gmail.com>
2021-10-27 21:35:04 +0700

    Fix dompet controller

--- app/Http/Controllers/Admin/DompetController.php
+++ app/Http/Controllers/Admin/DompetController.php
@@ -22,66 +22,14 @@ class DompetController extends Controller
      */
     public function index(Request $request)
     {
-        if ($request->ajax()) {
-            $dompets = Dompet::join('dompet_status', 'dompet.dompet_status_id', '=', 'dompet_status.status_id')
-            ->get(['dompet.*', 'dompet_status.status_name']);
-            $status = DompetStatus::all();
+        $dompets = Dompet::join('dompet_status', 'dompet.dompet_status_id', '=', 'dompet_status.status_id')
+        ->get(['dompet.dompet_id', 'dompet.dompet_name', 'dompet.dompet_reference', 'dompet.dompet_description', 'dompet.dompet_status_id', 'dompet_status.status_name']);
+        $status = DompetStatus::all();
 
-            return Datatables::of($dompets)
-                    ->addIndexColumn()
-                    ->addColumn('status', function($row){
-                         if($row->dompet_status_id){
-                            return '<span class="badge badge-primary">Aktif</span>';
-                         }else{
-                            return '<span class="badge badge-danger">Tidak Aktif</span>';
-                         }
-                    })
-                    ->filter(function ($instance) use ($request) {
-                        if ($request->get('status') == '0' || $request->get('status') == '1') {
-                            $instance->where('status_name', $request->get('status'));
-                        }
-                    })
-                    ->rawColumns(['status'])
-                    ->make(true);
+        if ($request->ajax()) {
+            return Datatables::of($dompets)->make(true);
         }
         
         return view('admin.dompet.index', compact('dompets', 'status'));
-    }
-
-    public function create(Dompet $dompets)
-    {
-        $status = DompetStatus::orderBy('name')->get();
-        return view('admin.dompet.create', compact('dompets', 'status'));
-    }
-
-    public function store(Request $request)
-    {
-        $validator = Validator::make($request->all(), 
-        [
-            'dompet_name' => 'required|min:5',
-            'dompet_deskripsi' => 'max:100'
-        ]);
-
-        if ($validator->fails()) {
-            return redirect()->route('admin.dompet.create')->withErrors($validator)->withInput();
-        }
-
-        $dompet_name = $request->get('dompet_name');
-        $dompet_referensi = $request->get('dompet_referensi');
-        $dompet_deskripsi = $request->get('dompet_deskripsi');
-        $dompet_status = $request->get('dompet_status');
-  
-        $execute = Dompet::create([
-            'name' => trim($dompet_name),
-            'reference' => trim($dompet_referensi),
-            'description' => trim($dompet_deskripsi),
-            'dompet_status_id' => intval($dompet_status)
-        ]);
-
-        if ($execute) {
-            return redirect()->route('admin.dompet.index')->with('success', 'Data dompet telah ditambahkan');
-        } else {
-            return redirect()->route('admin.dompet.index')->with('error', 'Terjadi kesalahan saat melakukan tambah data dompet!');
-        }
     }
 }
