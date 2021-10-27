e9847342d844f6cf96211e3c19d4e72e1694d74c
danielstputra <danielstputra@gmail.com>
2021-10-28 01:00:53 +0700

    Membuat halaman tambah kategori

---
+++ resources/views/admin/kategori/create.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.backend.app',[
+	'title' => 'Tambah Kategori',
+	'contentTitle' => 'Tambah Kategori',
+])
+
+@section('content')
+<div class="container-fluid">        
+	<div class="page-title">
+		<div class="row">
+			<div class="col-6">
+				<h3>{{ __('Tambah Kategori') }}</h3>
+			</div>
+			<div class="col-6">
+				<ol class="breadcrumb">
+				<li class="breadcrumb-item"><a href="{{ route('admin.index') }}">{{ __('Halaman Utama') }}</a></li>
+                <li class="breadcrumb-item"><a href="{{ route('admin.kategori.index') }}">{{ __('Kategori') }}</a></li>
+				<li class="breadcrumb-item active">{{ __('Tambah') }}</li>
+				</ol>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Container-fluid starts-->
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-sm-12 col-xl-12">
+			<div class="card">
+				<div class="card-body btn-showcase">
+					@include('flash-message')
+                    
+                    <form class="needs-validation" novalidate="" action="{{ route('admin.kategori.store') }}" method="POST" enctype="multipart/form-data">
+					@csrf
+                      <div class="row g-3 mb-3">
+                        <div class="col-md-12">
+							<label class="form-label" for="kategori_name">{{ __('Nama') }} <a style="color:red;">*</a></label>
+							<input class="form-control" type="text" id="kategori_name" name="kategori_name" value="{{ old('kategori_name') }}" placeholder="Nama" />
+                        </div>
+                      </div>
+
+					  <div class="mt-4 mb-3" style="border-bottom: 1px solid #ecf3fa"></div>
+
+                      <button class="btn btn-primary pull-right" type="submit">{{ __('Submit') }}</button>
+                    </form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- /.row -->
+</div>
+<!-- Container-fluid Ends-->
+@stop
