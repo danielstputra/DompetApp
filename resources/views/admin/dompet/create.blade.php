3cd987d2b3786fd4e32ff06fa8a7f6ca6b540686
danielstputra <danielstputra@gmail.com>
2021-10-27 18:02:40 +0700

    Membuat view tambah dompet

---
+++ resources/views/admin/dompet/create.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.backend.app',[
+	'title' => 'Tambah Dompet',
+	'contentTitle' => 'Tambah Dompet',
+])
+
+@section('content')
+<div class="container-fluid">        
+	<div class="page-title">
+		<div class="row">
+			<div class="col-6">
+				<h3>{{ __('Tambah Dompet') }}</h3>
+			</div>
+			<div class="col-6">
+				<ol class="breadcrumb">
+				<li class="breadcrumb-item"><a href="{{ route('admin.index') }}">{{ __('Halaman Utama') }}</a></li>
+                <li class="breadcrumb-item"><a href="{{ route('admin.dompet') }}">{{ __('Dompet') }}</a></li>
+				<li class="breadcrumb-item active">{{ __('Tambah') }}</li>
+				</ol>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Container-fluid starts-->
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-sm-12 col-xl-12">
+			<div class="card">
+				<div class="card-body btn-showcase">
+					@include('flash-message')
+                    
+                    <form class="needs-validation" novalidate="" action="{{ route('admin.users.store') }}" method="POST" enctype="multipart/form-data">
+					@csrf
+                      <div class="row g-3 mb-3">
+                        <div class="col-md-6">
+							<label class="form-label" for="dompet_name">{{ __('Nama : *') }}</label>
+							<input class="form-control" type="text" id="dompet_name" name="dompet_name" value="{{ old('dompet_name') }}" placeholder="Nama" />
+                        </div>
+
+                        <div class="col-md-6">
+							<label class="form-label" for="dompet_referensi">{{ __('Referensi : *') }}</label>
+							<input class="form-control" type="text" id="dompet_referensi" name="dompet_referensi" value="{{ old('dompet_referensi') }}" placeholder="Referensi" />
+                        </div>
+
+						<div class="col-md-12">
+							<label class="form-label" for="dompet_deskripsi">{{ __('Deskripsi : *') }}</label>
+							<textarea class="form-control" type="text" id="dompet_deskripsi" name="dompet_deskripsi" placeholder="Deskripsi">{{ old('dompet_deskripsi') }}</textarea>
+                        </div>
+
+						<div class="col-md-12">
+							<label class="form-label" for="dompet_status">{{ __('Status : *') }}</label>
+							<select class="form-select" id="dompet_status" name="dompet_status">
+								<option selected="" disabled="" value="">Pilih Status...</option>
+								<option value="1">Aktif</option>
+								<option value="0">Tidak Aktif</option>
+							</select>
+						</div>
+                      </div>
+
+					  <div class="mt-4 mb-3" style="border-bottom: 1px solid #ecf3fa"></div>
+
+                      <button class="btn btn-primary pull-right" type="submit">{{ __('Submit') }}</button>
+                    </form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- /.row -->
+</div>
+<!-- Container-fluid Ends-->
+@stop
