376e9696429ec7ab549c8be4c35bff30ef8ef6ad
danielstputra <danielstputra@gmail.com>
2021-10-27 19:58:50 +0700

    Fix model user, nama primary key

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,7 @@ class User extends Authenticatable
 
     public $table = "users";
 
-    protected $primaryKey = 'id';
+    protected $primaryKey = 'user_id';
     
     public $timestamps = true;
     
