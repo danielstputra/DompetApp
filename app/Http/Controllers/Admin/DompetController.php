88d2cf218db3c3613ac2abcafeefb29f796207d3
danielstputra <danielstputra@gmail.com>
2021-10-27 18:27:46 +0700

    Penambahan fungsi store untuk proses tambah dompet

--- app/Http/Controllers/Admin/DompetController.php
+++ app/Http/Controllers/Admin/DompetController.php
@@ -22,4 +22,36 @@ class DompetController extends Controller
     {
         return view('admin.dompet.create', compact('dompets'));
     }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), 
+        [
+            'dompet_name' => 'required|min:5',
+            'dompet_deskripsi' => 'max:100',
+            'dompet_status' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->route('admin.dompet.index')->withErrors($validator)->withInput();
+        }
+
+        $dompet_name = $request->get('dompet_name');
+        $dompet_referensi = $request->get('dompet_referensi');
+        $dompet_deskripsi = $request->get('dompet_deskripsi');
+        $dompet_status = $request->get('dompet_status');
+  
+        $execute = Dompet::create([
+            'name' => trim($dompet_name),
+            'reference' => trim($dompet_referensi),
+            'description' => trim($dompet_deskripsi),
+            'dompet_status_id' => intval($dompet_status)
+        ]);
+
+        if ($execute) {
+            return redirect()->route('admin.dompet.index')->with('success', 'Data dompet telah ditambahkan');
+        } else {
+            return redirect()->route('admin.dompet.index')->with('error', 'Terjadi kesalahan saat melakukan tambah data dompet!');
+        }
+    }
 }
