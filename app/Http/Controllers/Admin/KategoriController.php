ed3f8fdc631b311dbe3c5ef0c059867f10527d01
danielstputra <danielstputra@gmail.com>
2021-10-28 01:12:11 +0700

    Fix fungsi index pada controller kategori

--- app/Http/Controllers/Admin/KategoriController.php
+++ app/Http/Controllers/Admin/KategoriController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use DataTables;
+use App\Models\Kategori;
+use App\Models\KategoriStatus;
 
 class KategoriController extends Controller
 {
@@ -23,24 +26,23 @@ class KategoriController extends Controller
             ->addIndexColumn()
             ->filter(function ($instance) use ($request) {
                 if ($request->get('statusCode') == '1' || $request->get('statusCode') == '2') {
-                    $instance->where('kategori_status_id', $request->get('statusCode'));
+                    $instance->where('cat_status_id', $request->get('statusCode'));
                 } else {
-                    $instance->where('kategori_status_id', '!=', $request->get('statusCode'));
+                    $instance->where('cat_status_id', '!=', $request->get('statusCode'));
                 }
 
                 if (!empty($request->get('search'))) {
                     $instance->where(function($w) use($request){
                        $search = $request->get('search');
-                       $w->orWhere('kategori_name', 'LIKE', "%$search%")
-                       ->orWhere('kategori_reference', 'LIKE', "%$search%")
-                       ->orWhere('kategori_description', 'LIKE', "%$search%");
+                       $w->orWhere('cat_name', 'LIKE', "%$search%")
+                       ->orWhere('cat_description', 'LIKE', "%$search%");
                    });
                }
             })
-            ->addColumn('kategori_status_id', function($row) {
-                if ($row->kategori_status_id == '1') {
+            ->addColumn('cat_status_id', function($row) {
+                if ($row->cat_status_id == '1') {
                     return 'Aktif';
-                } elseif($row->kategori_status_id == '2') {
+                } elseif($row->cat_status_id == '2') {
                     return 'Tidak Aktif';
                 } else {
                     return '';
@@ -48,9 +50,9 @@ class KategoriController extends Controller
             })
             ->addColumn('action', function($row){
                 $status = '';
-                if ($row->kategori_status_id != '1') {
+                if ($row->cat_status_id != '1') {
                     $status = 'Aktif';
-                } elseif($row->kategori_status_id != '2') {
+                } elseif($row->cat_status_id != '2') {
                     $status = 'Tidak Aktif';
                 } else {
                     $status = '';
@@ -63,15 +65,15 @@ class KategoriController extends Controller
                     </button>
 
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">'.$row->kategori_name.'</a></li>
+                        <li><a class="dropdown-item" href="#">'.$row->cat_name.'</a></li>
                         <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="'.route('admin.kategori.show', $row->kategori_id).'">Detail</a></li>
-                        <li><a class="dropdown-item" href="'.route('admin.kategori.edit', $row->kategori_id).'">Ubah</a></li>
+                        <li><a class="dropdown-item" href="'.route('admin.kategori.show', $row->cat_id).'">Detail</a></li>
+                        <li><a class="dropdown-item" href="'.route('admin.kategori.edit', $row->cat_id).'">Ubah</a></li>
                         <li><a class="dropdown-item" href="#">'.$status.'</a></li>
                     </ul>
                 </div>';
             })
-            ->rawColumns(['kategori_status_id', 'action'])
+            ->rawColumns(['cat_status_id', 'action'])
             ->make(true);
         }
         
