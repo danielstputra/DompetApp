b5b62f7f327dcd09b13cd203fd8572bad35793bc
danielstputra <danielstputra@gmail.com>
2021-10-27 21:51:14 +0700

    Fix dompet controller

--- app/Http/Controllers/Admin/DompetController.php
+++ app/Http/Controllers/Admin/DompetController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
+use DataTables;
 use App\Models\Dompet;
 use App\Models\DompetStatus;
 
@@ -27,7 +28,32 @@ class DompetController extends Controller
         $status = DompetStatus::all();
 
         if ($request->ajax()) {
-            return Datatables::of($dompets)->make(true);
+            return Datatables::of($dompets)
+            ->addIndexColumn()
+            ->addColumn('status', function($row){
+                if($row->status){
+                    return '<span class="badge badge-primary">Active</span>';
+                }else{
+                    return '<span class="badge badge-danger">Deactive</span>';
+                }
+            })
+            ->filter(function ($instance) use ($request) {
+                if ($request->get('status') == '0' || $request->get('status') == '1' || $request->get('status') == '2') {
+                    $instance->where('status', $request->get('status'));
+                }
+
+                if (!empty($request->get('search'))) {
+                        $instance->where(function($w) use($request){
+                        $search = $request->get('search');
+
+                        $w->orWhere('name', 'LIKE', "%$search%")
+                        ->orWhere('reference', 'LIKE', "%$search%")
+                        ->orWhere('description', 'LIKE', "%$search%");
+                    });
+                }
+            })
+            ->rawColumns(['status'])
+            ->make(true);
         }
         
         return view('admin.dompet.index', compact('dompets', 'status'));
