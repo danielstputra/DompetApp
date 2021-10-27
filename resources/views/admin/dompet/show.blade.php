437a48528e55532cf9a1012ffe673326defc1a5f
danielstputra <danielstputra@gmail.com>
2021-10-28 00:13:47 +0700

    Fix view dompet, menampilkan data sesuai id

--- resources/views/admin/dompet/show.blade.php
+++ resources/views/admin/dompet/show.blade.php
@@ -26,7 +26,42 @@
 		<div class="col-sm-12 col-xl-12">
 			<div class="card">
 				<div class="card-body btn-showcase">
-
+                    <div class="table-responsive" id="table">
+                        <table class="table table-bordered table-striped">
+                            <tbody>
+                                <tr>
+                                    <td><h6 class="p-2 mb-0">NAMA</h6></td>
+                                    <td><p class="m-0">{{ $dompet->dompet_name }}</p></td>
+                                </tr>
+                                <tr>
+                                    <td><h6 class="p-2 mb-0">REFERENSI</h6></td>
+                                    <td><p class="m-0">{{ $dompet->dompet_reference }}</p></td>
+                                </tr>
+                                <tr>
+                                    <td><h6 class="p-2 mb-0">DESKRIPSI</h6></td>
+                                    <td><p class="m-0">{{ $dompet->dompet_description }}</p></td>
+                                </tr>
+                                <tr>
+                                    <td><h6 class="p-2 mb-0">STATUS</h6></td>
+                                    @if ($dompet->dompet_status_id == 1)
+                                        <td><p class="m-0">{{ __('Aktif') }}</p></td>
+                                    @elseif ($dompet->dompet_status_id == 2)
+                                        <td><p class="m-0">{{ __('Aktif') }}</p></td>
+                                    @else
+                                        <td><p class="m-0">{{ __('-') }}</p></td>
+                                    @endif
+                                </tr>
+                                <tr>
+                                    <td><h6 class="p-2 mb-0">TANGGAL DIBUAT</h6></td>
+                                    <td><p class="m-0">{{ $dompet->created_at }}</p></td>
+                                </tr>
+                                <tr>
+                                    <td><h6 class="p-2 mb-0">TANGGAL DIUBAH</h6></td>
+                                    <td><p class="m-0">{{ $dompet->updated_at }}</p></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
 				</div>
 			</div>
 		</div>
