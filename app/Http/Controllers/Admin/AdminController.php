1680b4f983c436bc40b96d6a8df0438bf639afe3
danielstputra <danielstputra@gmail.com>
2021-10-27 15:50:53 +0700

    Update Admin Controller

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -17,6 +17,7 @@ class AdminController extends Controller
 
     public function index()
     {
+        //
         $users = User::where('id', Auth::user()->id)->first();
         return view('admin.dashboard', compact('users'));
     }
