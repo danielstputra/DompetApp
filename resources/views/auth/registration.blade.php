e859e533176bd124d3816fbf2f0fff3c0b50ee10
danielstputra <danielstputra@gmail.com>
2021-10-28 19:41:34 +0700

    Update css dan javascript

--- resources/views/auth/registration.blade.php
+++ resources/views/auth/registration.blade.php
@@ -1,4 +1,13 @@
 
+<!-----------------------------------------------------------------------------------
+
+    Template Name: CyberFrost Modern Theme
+    Template Version : 1.0.1
+    -----------------------------------------------------
+    Author: Daniels Trysyahputra
+    Author URI: https://motonet.id/
+
+----------------------------------------------------------------------------------->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -6,43 +15,43 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Cuba admin is super flexible, powerful, clean &amp; modern responsive bootstrap 5 admin template with unlimited possibilities.">
-    <meta name="keywords" content="admin template, Cuba admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="description" content="CyberFrost admin is super flexible, powerful, clean &amp; modern responsive bootstrap 5 admin template with unlimited possibilities.">
+    <meta name="keywords" content="admin template, CyberFrost Modern Theme, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="pixelstrap">
-    <link rel="icon" href="{{ asset('templates/backend/CubeTheme') }}/images/favicon.png" type="image/x-icon">
-    <link rel="shortcut icon" href="{{ asset('templates/backend/CubeTheme') }}/images/favicon.png" type="image/x-icon">
+    <link rel="icon" href="{{ asset('templates/backend/CyberFrostModernTheme') }}/images/favicon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('templates/backend/CyberFrostModernTheme') }}/images/favicon.png" type="image/x-icon">
     <title>{{ $config->app_name }} | Register Akun</title>
     <!-- Google font-->
     <link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i&amp;display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900&amp;display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="{{ asset('templates/backend/CubeTheme') }}/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('templates/backend/CyberFrostModernTheme') }}/css/font-awesome.css">
     <!-- ico-font-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('templates/backend/CubeTheme') }}/css/vendors/icofont.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('templates/backend/CyberFrostModernTheme') }}/css/vendors/icofont.css">
     <!-- Themify icon-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('templates/backend/CubeTheme') }}/css/vendors/themify.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('templates/backend/CyberFrostModernTheme') }}/css/vendors/themify.css">
     <!-- Flag icon-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('templates/backend/CubeTheme') }}/css/vendors/flag-icon.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('templates/backend/CyberFrostModernTheme') }}/css/vendors/flag-icon.css">
     <!-- Feather icon-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('templates/backend/CubeTheme') }}/css/vendors/feather-icon.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('templates/backend/CyberFrostModernTheme') }}/css/vendors/feather-icon.css">
     <!-- Plugins css start-->
     <!-- Plugins css Ends-->
     <!-- Bootstrap css-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('templates/backend/CubeTheme') }}/css/vendors/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('templates/backend/CyberFrostModernTheme') }}/css/vendors/bootstrap.css">
     <!-- App css-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('templates/backend/CubeTheme') }}/css/style.css">
-    <link id="color" rel="stylesheet" href="{{ asset('templates/backend/CubeTheme') }}/css/color-1.css" media="screen">
+    <link rel="stylesheet" type="text/css" href="{{ asset('templates/backend/CyberFrostModernTheme') }}/css/style.css">
+    <link id="color" rel="stylesheet" href="{{ asset('templates/backend/CyberFrostModernTheme') }}/css/color-1.css" media="screen">
     <!-- Responsive css-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('templates/backend/CubeTheme') }}/css/responsive.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('templates/backend/CyberFrostModernTheme') }}/css/responsive.css">
   </head>
   <body>
     <!-- login page start-->
     <div class="container-fluid">
       <div class="row m-0">
-        <div class="col-xl-5"><img class="bg-img-cover bg-center" src="{{ asset('templates/backend/CubeTheme') }}/images/login/3.jpg" alt="looginpage"></div>
+        <div class="col-xl-5"><img class="bg-img-cover bg-center" src="{{ asset('templates/backend/CyberFrostModernTheme') }}/images/login/3.jpg" alt="looginpage"></div>
         <div class="col-xl-7 p-0"> 
           <div class="login-card">
             <div>
-              <div><a class="logo" href="index-2.html"><img class="img-fluid for-light" src="{{ asset('templates/backend/CubeTheme') }}/images/logo/login.png" alt="looginpage"><img class="img-fluid for-dark" src="{{ asset('templates/backend/CubeTheme') }}/images/logo/logo_dark.png" alt="looginpage"></a></div>
+              <div><a class="logo" href="index-2.html"><img class="img-fluid for-light" src="{{ asset('templates/backend/CyberFrostModernTheme') }}/images/logo/login.png" alt="looginpage"><img class="img-fluid for-dark" src="{{ asset('templates/backend/CyberFrostModernTheme') }}/images/logo/logo_dark.png" alt="looginpage"></a></div>
               <div class="login-main">
                 @include('flash-message')
                 <form class="theme-form" action="{{ route('register.post') }}" method="POST">
@@ -122,19 +131,19 @@
         </div>
       </div>
       <!-- latest jquery-->
-      <script src="{{ asset('templates/backend/CubeTheme') }}/js/jquery-3.5.1.min.js"></script>
+      <script src="{{ asset('templates/backend/CyberFrostModernTheme') }}/js/jquery-3.5.1.min.js"></script>
       <!-- Bootstrap js-->
-      <script src="{{ asset('templates/backend/CubeTheme') }}/js/bootstrap/bootstrap.bundle.min.js"></script>
+      <script src="{{ asset('templates/backend/CyberFrostModernTheme') }}/js/bootstrap/bootstrap.bundle.min.js"></script>
       <!-- feather icon js-->
-      <script src="{{ asset('templates/backend/CubeTheme') }}/js/icons/feather-icon/feather.min.js"></script>
-      <script src="{{ asset('templates/backend/CubeTheme') }}/js/icons/feather-icon/feather-icon.js"></script>
+      <script src="{{ asset('templates/backend/CyberFrostModernTheme') }}/js/icons/feather-icon/feather.min.js"></script>
+      <script src="{{ asset('templates/backend/CyberFrostModernTheme') }}/js/icons/feather-icon/feather-icon.js"></script>
       <!-- scrollbar js-->
       <!-- Sidebar jquery-->
-      <script src="{{ asset('templates/backend/CubeTheme') }}/js/config.js"></script>
+      <script src="{{ asset('templates/backend/CyberFrostModernTheme') }}/js/config.js"></script>
       <!-- Plugins JS start-->
       <!-- Plugins JS Ends-->
       <!-- Theme js-->
-      <script src="{{ asset('templates/backend/CubeTheme') }}/js/script.js"></script>
+      <script src="{{ asset('templates/backend/CyberFrostModernTheme') }}/js/script.js"></script>
       <!-- login js-->
       <!-- Plugin used-->
     </div>
