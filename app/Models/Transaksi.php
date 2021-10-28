270c4ae204bcb91ca534df679d9ede673723bdcb
danielstputra <danielstputra@gmail.com>
2021-10-28 09:58:56 +0700

    Membuat fungsi prefix auto_increment

--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -28,4 +28,30 @@ class Transaksi extends Model
         'cat_id',
         'trx_status_id'
     ];
+
+    //function membuat format id baru berbentuk 00001
+    public function get_newid($auto_id){
+        $newId = substr($auto_id, 1,4);
+        $tambah = (int)$newId + 1;
+
+        if (strlen($tambah) == 1){
+            $id_mobil = "00000" .$tambah;
+        }
+        else if (strlen($tambah) == 2){
+            $id_mobil = "0000" .$tambah;
+        }
+        else if(strlen($tambah) == 3){
+            $id_mobil = "000".$tambah;   
+        }
+        else if (strlen($tambah) == 4){
+            $id_mobil = "00" .$tambah;
+        }
+        else if(strlen($tambah) == 5){
+            $id_mobil = "0" .$tambah;
+        }
+        else if(strlen($tambah) == 6){
+            $id_mobil = $tambah;   
+        }
+        return $id_mobil;
+    }
 }
