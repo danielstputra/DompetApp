01fc5aae045f8e83b08813098a6bc5704e9e3326
danielstputra <danielstputra@gmail.com>
2021-10-28 12:36:44 +0700

    Fix view laporan transaksi

--- app/Http/Controllers/Admin/LaporanTransaksiController.php
+++ app/Http/Controllers/Admin/LaporanTransaksiController.php
@@ -14,6 +14,6 @@ class LaporanTransaksiController extends Controller
 
     public function index()
     {
-        return view('admin.laporan.index');
+        return view('admin.laporan');
     }
 }
