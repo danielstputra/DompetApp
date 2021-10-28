659a853d730e16a1e16ed8a0cae727e9bcc4fc9b
danielstputra <danielstputra@gmail.com>
2021-10-28 12:34:44 +0700

    Membuat fungsi index untuk view halaman laporan

--- app/Http/Controllers/Admin/LaporanTransaksiController.php
+++ app/Http/Controllers/Admin/LaporanTransaksiController.php
@@ -7,5 +7,13 @@ use Illuminate\Http\Request;
 
 class LaporanTransaksiController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware(['auth','is_verified']);
+    }
+
+    public function index()
+    {
+        return view('admin.laporan.index');
+    }
 }
